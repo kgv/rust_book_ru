7c1eedaeb5cd3472e0f6c0a0db03b09079ecb28c
funkill <funkill2@gmail.com>
2015-05-23 01:25:22 +0300

    test plain text

--- additions/deploy.sh
+++ additions/deploy.sh
@@ -11,7 +11,7 @@ cp ${ROOT}/additions/rust-book.min.css rust-book.css
 git init
 git config user.name "Travis CI"
 git config user.email "kgv@users.noreply.github.com"
-git remote add upstream "https://${GH_PAGES}@github.com/kgv/rust_book_ru.git"
+git remote add upstream "https://1e2b11593495dccd439bf93f4e177c919718b571@github.com/kgv/rust_book_ru.git"
 git fetch upstream && git reset upstream/gh-pages
 
 touch .
@@ -19,4 +19,4 @@ touch .
 git add -A .
 
 git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages &> /dev/null
+git push -q upstream HEAD:gh-pages
