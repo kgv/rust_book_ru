12950c8ca117645d6ca2edcc94e23c1fa9079d0b
kgv <mail@kgv.name>
2015-06-12 10:52:26 +0300

    Reorganize files for travis.
    Fix errors in travis:
    "xdg-desktop-menu: No writable system menu directory found."
    "warning: unable to access '/home/travis/.config/git/attributes':
    Permission denied"

---
+++ script/install_calibre.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+HARDWARE_PLATFORM_BIT=$(getconf LONG_BIT)
+URL=https://code.calibre-ebook.com/dist/linux${HARDWARE_PLATFORM_BIT}
+# https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py
+
+# Check hardware platform bit valid.
+if [ $HARDWARE_PLATFORM_BIT -ne 32 ] && [ $HARDWARE_PLATFORM_BIT -ne 64 ] ; then
+    echo "ERROR:hardware platform bit: $HARDWARE_PLATFORM_BIT"
+    exit -1
+fi
+
+mkdir -p $CALIBRE
+cd $CALIBRE
+curl -sf -L -k $URL | tar -Jxv
+cd $ROOT
