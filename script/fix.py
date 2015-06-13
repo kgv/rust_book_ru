fd02ad52f899f4c86e18f2e94e5bc31b92d6e8a3
kgv <mail@kgv.name>
2015-06-13 10:19:25 +0300

    Fix check line width

--- script/fix.py
+++ script/fix.py
@@ -21,9 +21,6 @@ def fix_header(file):
     return result
 
 for file in glob.glob("./*.md"):
-    fixs = {}
     result = fix_header(file)
-    fixs["fix_header"] = result
-
-    if fixs["fix_header"]:
-        logging.info("fix header ok: file: {}, fix header: {}".format(file, fixs["fix_header"]))
+    if result:
+        logging.info("fix header: ok, file: {}, count: {}".format(file, result))
