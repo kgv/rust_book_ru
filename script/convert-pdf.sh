5be63e8b5f24a8826f1e59b7f99c3c9564ec2524
Michael Pankov <work@michaelpankov.com>
2015-07-11 22:28:06 +0300

    Move pdf conversion

---
+++ script/convert-pdf.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+BOOK_DIR=$(pwd)
+CONVERTED_DIR=${BOOK_DIR}/converted
+
+mkdir -p $CONVERTED_DIR
+
+ebook-convert \
+    README.html \
+    "${CONVERTED_DIR}/rustbook.pdf" \
+    --cover="../cover.jpg" \
+    --title="Язык программирования Rust" \
+    --comments="" \
+    --language="ru" \
+    --book-producer="" \
+    --publisher="" \
+    --chapter="//h:h1[@class='title']" \
+    --chapter-mark="pagebreak" \
+    --page-breaks-before="/" \
+    --level1-toc="//h:h1[@class='title']" \
+    --no-chapters-in-toc \
+    --max-levels="1" \
+    --breadth-first \
+    --margin-left="62" \
+    --margin-right="62" \
+    --margin-top="56" \
+    --margin-bottom="56" \
+    --pdf-default-font-size="16" \
+    --pdf-mono-font-size="16" \
+    --paper-size="a4" \
+    --pdf-header-template="<p class='header'><span>Язык программирования Rust</span></p>" \
+    --pdf-footer-template="<p class='footer'><span>_SECTION_</span> <span style='float:right;'>_PAGENUM_</span></p>"
--- script/convert.sh
+++ script/convert.sh
@@ -10,32 +10,6 @@ cp ${ROOT}/static/css/rust-book-pdf.min.css rust-book.css
 
 "$(${ROOT}/script/convert-epub.sh)"
 "$(${ROOT}/script/convert-mobi.sh)"
-
-
-ebook-convert \
-    README.html \
-    "${CONVERTED_DIR}/rustbook.pdf" \
-    --cover="../cover.jpg" \
-    --title="Язык программирования Rust" \
-    --comments="" \
-    --language="ru" \
-    --book-producer="" \
-    --publisher="" \
-    --chapter="//h:h1[@class='title']" \
-    --chapter-mark="pagebreak" \
-    --page-breaks-before="/" \
-    --level1-toc="//h:h1[@class='title']" \
-    --no-chapters-in-toc \
-    --max-levels="1" \
-    --breadth-first \
-    --margin-left="62" \
-    --margin-right="62" \
-    --margin-top="56" \
-    --margin-bottom="56" \
-    --pdf-default-font-size="16" \
-    --pdf-mono-font-size="16" \
-    --paper-size="a4" \
-    --pdf-header-template="<p class='header'><span>Язык программирования Rust</span></p>" \
-    --pdf-footer-template="<p class='footer'><span>_SECTION_</span> <span style='float:right;'>_PAGENUM_</span></p>"
+"$(${ROOT}/script/convert-pdf.sh)"
 
 cd $ROOT
