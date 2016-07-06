82e5b22c7fc81733a3c99529b36a1c411ee99bf1
Michael Pankov <work@michaelpankov.com>
2016-07-06 16:45:08 +0300

    Use Steve's rustbook

--- script/install_rustbook.sh
+++ script/install_rustbook.sh
@@ -4,7 +4,7 @@ SAVED_PWD=$(pwd)
 export RUSTBOOK=$HOME/rustbook; echo "INFO:RUSTBOOK: $RUSTBOOK"
 
 if [ ! -f "$RUSTBOOK"/target/release/rustbook ]; then
-    git clone https://github.com/mkpankov/rustbook $RUSTBOOK
+    git clone https://github.com/steveklabnik/rustbook $RUSTBOOK
     cd $RUSTBOOK
     git checkout e19eebb99443a6bf3f88e3f4e6b1dd21d3f3bf05
     cargo build --release
