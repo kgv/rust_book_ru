60fcb56076fb7c4a7bfd61dd5ae36f154ce45fa0
Michael Pankov <work@michaelpankov.com>
2016-07-24 12:30:34 +0300

    Fix the path to script

--- script/build.sh
+++ script/build.sh
@@ -7,4 +7,5 @@ sed "s/<revision>/$REV/g" README.rev.template.md >> README.md
 rustbook build
 
 sed -i "s/@import url('..\/rust.css');/@import url('.\/rust.css');/g" _book/rustbook.css
+sed -i "s/<script src='rustbook.js'><\/script>/<script src='..\/rustbook.js'><\/script>/g" $(find _book/src -type f)
 cp static/css/rust.css _book
