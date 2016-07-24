a1f19210faae753942e5557d753d3db42ef0b317
Michael Pankov <work@michaelpankov.com>
2016-07-24 12:44:52 +0300

    Исправляем запуск примеров в браузере
    Fix #26

--- script/build.sh
+++ script/build.sh
@@ -8,4 +8,5 @@ rustbook build
 
 sed -i "s/@import url('..\/rust.css');/@import url('.\/rust.css');/g" _book/rustbook.css
 sed -i "s/<script src='rustbook.js'><\/script>/<script src='..\/rustbook.js'><\/script>/g" $(find _book/src -type f)
+sed -i "s/<script src='playpen.js'><\/script>/<script src='..\/playpen.js'><\/script>/g" $(find _book/src -type f)
 cp static/css/rust.css _book
